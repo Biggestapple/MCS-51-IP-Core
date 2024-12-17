bcbc0f51d3c304b3357b345f1eb397594851c700
unknown <15021699275@163.com>
2024-12-17 21:55:07 +0800

    Add several MOV_group instruction

--- rtl/mc8051/mc8051_op_decoder.v
+++ rtl/mc8051/mc8051_op_decoder.v
@@ -16,52 +16,68 @@
 module op_decoder(
 	input		[7:0]					i_instr_buffer,
 	input		[1:0]					i_ci_stage,
+    input                               i_s1_done_tick,
+    input                               i_s2_done_tick,
+    input                               i_s3_done_tick,
 	
-	output	reg	[`MCODE_WIDTH-1:0]		o_mc_b
+	output	reg	[`MCODE_WIDTH-1:0]      o_mc_b
 );
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------//
-`define                 SIMULATION_VE
+`define                 EN_SIMULATION
 /*
 `define                 NO_SIMULATION
 */
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------//
-`ifdef SIMULATION_VE
-    if(i_ci_stage == 2'b00)
-        case(i_instr_buffer)
-            `NOP:   o_mc_b  =   
+`ifdef EN_SIMULATION
+    always @(*)
+        if(i_ci_stage == 2'b00)
+            casez(i_instr_buffer)
+                `NOP            :   o_mc_b  =   {1'b0,`FROM_NULL_0,`TO_IDLE_0,`PC_NUL_RELOAD,`JP_IDLE_0,`PSW_M0_RELOAD,`ALU_IN1_S2B,`ALU_I0_ACC,`ALU_IDLE_0,`S5_ADDR_INDX8,`WR_DISCARD_MODE,`S5_WR_S2B,`S3_ADDR_PC,`DISCARD_MODE,1'b0,`S2_ADDR_PC,`DISCARD_MODE,1'b0};
+                `MOV_A_IMM      :   o_mc_b  =   {1'b0,`FROM_S2_BUF,`TO_ACC_REG,`PC_NUL_RELOAD,`JP_IDLE_0,`PSW_M0_RELOAD,`ALU_IN1_S2B,`ALU_I0_ACC,`ALU_IDLE_0,`S5_ADDR_INDX8,`WR_DISCARD_MODE,`S5_WR_S2B,`S3_ADDR_PC,`DISCARD_MODE,1'b0,`S2_ADDR_PC,`IND_EXROM_MODE,1'b1};
+                `MOV_A_RN       :   o_mc_b  =   {1'b0,`FROM_S2_BUF,`TO_ACC_REG,`PC_NUL_RELOAD,`JP_IDLE_0,`PSW_M0_RELOAD,`ALU_IN1_S2B,`ALU_I0_ACC,`ALU_IDLE_0,`S5_ADDR_INDX8,`WR_DISCARD_MODE,`S5_WR_S2B,`S3_ADDR_PC,`DISCARD_MODE,1'b0,`S2_ADDR_RS0,`DIR_IRAM_MODE,1'b0};
+                `MOV_A_DIR      :   o_mc_b  =   {1'b0,`FROM_S3_BUF,`TO_ACC_REG,`PC_NUL_RELOAD,`JP_IDLE_0,`PSW_M0_RELOAD,`ALU_IN1_S2B,`ALU_I0_ACC,`ALU_IDLE_0,`S5_ADDR_INDX8,`WR_DISCARD_MODE,`S5_WR_S2B,`S3_ADDR_INDX8,`DIR_IRAM_MODE,1'b0,`S2_ADDR_PC,`IND_EXROM_MODE,1'b1};
+                `MOV_A_F_RN     :   o_mc_b  =   {1'b0,`FROM_S3_BUF,`TO_ACC_REG,`PC_NUL_RELOAD,`JP_IDLE_0,`PSW_M0_RELOAD,`ALU_IN1_S2B,`ALU_I0_ACC,`ALU_IDLE_0,`S5_ADDR_INDX8,`WR_DISCARD_MODE,`S5_WR_S2B,`S3_ADDR_INDX8,`IND_IRAM_MODE,1'b0,`S2_ADDR_RS1,`DIR_IRAM_MODE,1'b0};
+                `MOV_RN_A       :   o_mc_b  =   {1'b0,`FROM_NULL_0,`TO_IDLE_0,`PC_NUL_RELOAD,`JP_IDLE_0,`PSW_M0_RELOAD,`ALU_IN1_S2B,`ALU_I0_ACC,`ALU_IDLE_0,`S5_ADDR_RS0,`WR_DIR_2IRAM_MODE,`S5_WR_ACC,`S3_ADDR_PC,`DISCARD_MODE,1'b0,`S2_ADDR_PC,`DISCARD_MODE,1'b0};
+                `MOV_RN_DIR     :   o_mc_b  =   {1'b0,`FROM_NULL_0,`TO_IDLE_0,`PC_NUL_RELOAD,`JP_IDLE_0,`PSW_M0_RELOAD,`ALU_IN1_S2B,`ALU_I0_ACC,`ALU_IDLE_0,`S5_ADDR_RS0,`WR_DIR_2IRAM_MODE,`S5_WR_S3B,`S3_ADDR_INDX8,`DIR_IRAM_MODE,1'b0,`S2_ADDR_PC,`IND_EXROM_MODE,1'b1};
+                `MOV_RN_IMM     :   o_mc_b  =   {1'b0,`FROM_NULL_0,`TO_IDLE_0,`PC_NUL_RELOAD,`JP_IDLE_0,`PSW_M0_RELOAD,`ALU_IN1_S2B,`ALU_I0_ACC,`ALU_IDLE_0,`S5_ADDR_RS0,`WR_DIR_2IRAM_MODE,`S5_WR_S2B,`S3_ADDR_PC,`DISCARD_MODE,1'b0,`S2_ADDR_PC,`IND_EXROM_MODE,1'b1};
+                `MOV_DIR_A      :   o_mc_b  =   {1'b0,`FROM_NULL_0,`TO_IDLE_0,`PC_NUL_RELOAD,`JP_IDLE_0,`PSW_M0_RELOAD,`ALU_IN1_S2B,`ALU_I0_ACC,`ALU_IDLE_0,`S5_ADDR_INDX8,`WR_DIR_2IRAM_MODE,`S5_WR_ACC,`S3_ADDR_PC,`DISCARD_MODE,1'b0,`S2_ADDR_PC,`IND_EXROM_MODE,1'b1};
+                `MOV_DIR_RN     :   o_mc_b  =   {1'b1,`FROM_S2_BUF,`TO_SX0_REG,`PC_NUL_RELOAD,`JP_IDLE_0,`PSW_M0_RELOAD,`ALU_IN1_S2B,`ALU_I0_ACC,`ALU_IDLE_0,`S5_ADDR_INDX8,`WR_DISCARD_MODE,`S5_WR_S2B,`S3_ADDR_PC,`IND_EXROM_MODE,1'b1,`S2_ADDR_RS0,`DIR_IRAM_MODE,1'b0};
+                default:
+                    begin
+                        o_mc_b  =   {`MCODE_WIDTH{1'bz}};
+                        if(i_s1_done_tick |i_s2_done_tick|i_s3_done_tick)
+                            $display ("%m :at time %t Warning! In ci_stage %d, fetched invalid op_code %h return 8'bzzzz_zzzz.", $time,	i_ci_stage, i_instr_buffer);
+                    end
+            endcase
+        else if(i_ci_stage == 2'b01)
+            casez(i_instr_buffer)
+                `MOV_DIR_RN     :   o_mc_b  =   {1'b0,`FROM_NULL_0,`TO_IDLE_0,`PC_NUL_RELOAD,`JP_IDLE_0,`PSW_M0_RELOAD,`ALU_IN1_S2B,`ALU_I0_ACC,`ALU_IDLE_0,`S5_ADDR_SINDX8,`WR_DIR_2IRAM_MODE,`S5_WR_SX0,`S3_ADDR_PC,`DISCARD_MODE,1'b0,`S2_ADDR_PC,`DISCARD_MODE,1'b0};
+                default:
+                    begin
+                        o_mc_b  =   {`MCODE_WIDTH{1'bz}};
+                        if(i_s1_done_tick |i_s2_done_tick|i_s3_done_tick)
+                            $display ("%m :at time %t Warning! In ci_stage %d, fetched invalid op_code %h return 8'bzzzz_zzzz.", $time,	i_ci_stage, i_instr_buffer);
+                    end
+            endcase
+        else if(i_ci_stage == 2'b10)
+            casez(i_instr_buffer)
 
-            default:
-                begin
-                    o_mc_b  =   {`MCODE_WIDTH{1'bz}};
-                   $display ("%m :at time %t Warning! In ci_stage %d, fetched invalid op_code %h return 8'bzzzz_zzzz.", $time,	i_ci_stage, i_instr_buffer);
-                end
-        endcase
-    else if(i_ci_stage == 2'b01)
-        case(i_instr_buffer)
+                default:
+                    begin
+                        o_mc_b  =   {`MCODE_WIDTH{1'bz}};
+                        if(i_s1_done_tick |i_s2_done_tick|i_s3_done_tick)
+                            $display ("%m :at time %t Warning! In ci_stage %d, fetched invalid op_code %h return 8'bzzzz_zzzz.", $time,	i_ci_stage, i_instr_buffer);
+                    end
+            endcase
+        else
+            casez(i_instr_buffer)
 
-            default:
-                begin
-                    o_mc_b  =   {`MCODE_WIDTH{1'bz}};
-                   $display ("%m :at time %t Warning! In ci_stage %d, fetched invalid op_code %h return 8'bzzzz_zzzz.", $time,	i_ci_stage, i_instr_buffer);
-                end
-        endcase
-    else if(i_ci_stage == 2'b10)
-        case(i_instr_buffer)
-
-            default:
-                begin
-                    o_mc_b  =   {`MCODE_WIDTH{1'bz}};
-                    $display ("%m :at time %t Warning! In ci_stage %d, fetched invalid op_code %h return 8'bzzzz_zzzz.", $time,	i_ci_stage, i_instr_buffer);
-                end
-        endcase
-    else
-        case(i_instr_buffer)
-
-            default:
-                begin
-                    o_mc_b  =   {`MCODE_WIDTH{1'bz}};
-                    $display ("%m :at time %t Warning! In ci_stage %d, fetched invalid op_code %h return 8'bzzzz_zzzz.", $time,	i_ci_stage, i_instr_buffer);                end
-        endcase
+                default:
+                    begin
+                        o_mc_b  =   {`MCODE_WIDTH{1'bz}};
+                        if(i_s1_done_tick |i_s2_done_tick|i_s3_done_tick)
+                            $display ("%m :at time %t Warning! In ci_stage %d, fetched invalid op_code %h return 8'bzzzz_zzzz.", $time,	i_ci_stage, i_instr_buffer);                end
+            endcase
 `else
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------//
     localparam			BASIC_FILENAME	=	"MicroCodeTable.hex";
